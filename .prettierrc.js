fd118af095a351f86ab11ebd325d1914cf3d4cf7
ohtaekwon <ohtk1119@gmail.com>
2024-03-20 14:10:36 +0900

    feat prettier -> prettier.js 파일로 변경

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports ={
+  "printWidth": 180, // 한 줄에 출력되는 코드의 최대 길이
+  tabWidth: 2, // 탭 간격의 크기를 설정
+  "endOfLine": "auto", // 줄 바꿈 문자열을 지정
+  "singleQuote": true,  //  문자열을 작은 따옴표로 표시
+  "trailingComma": "all", // 후행 쉼표를 모든 요소에 적용
+  arrowParens: 'avoid', // 화살표 함수의 매개변수가 하나일 때 괄호를 생략하도록 설정
+
+}
