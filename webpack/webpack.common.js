9c6f960304f94368173a4226a9def2520426fee7
ohtaekwon <ohtk1119@gmail.com>
2024-03-20 13:57:44 +0900

    feat update module's rules to write import about style's file

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,42 +1,53 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '../dist'),
+    clean: true,
+  },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.(js|ts|tsx)$/i,
-        use: ["babel-loader", "ts-loader"],
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
           },
         ],
       },
     ],
   },
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: '../public/index.html',
     }),
     new CleanWebpackPlugin(),
   ],
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
     hot: true,
     open: true,
   },
