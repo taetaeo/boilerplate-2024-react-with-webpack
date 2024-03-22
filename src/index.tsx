7082a8391536b49542efcbc67d175ce2788f70be
ohtaekwon <ohtk1119@gmail.com>
2024-03-22 13:57:59 +0900

    feat Connect mocking api

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('@mocks/browser/worker');
+  return worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+
+enableMocking().then(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
