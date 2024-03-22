12064e710d5576dcc21de4c16fe1ed4ed6dc027e
ohtaekwon <ohtk1119@gmail.com>
2024-03-22 13:58:37 +0900

    chore Add babel config

--- bable.config.js
+++ bable.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-typescript'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@commons': './src/_commons',
+          '@mocks': './src/_mocks',
+          '@components': './src/components',
+          '@configs': './src/configs',
+          '@dto': './src/dto',
+          '@hooks': './src/hooks',
+          '@hocs': './src/hocs',
+          '@lib': './src/lib',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
