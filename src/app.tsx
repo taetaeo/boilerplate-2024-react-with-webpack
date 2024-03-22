97f9140c864ebb29c2283f25461e7dbd9338a85f
ohtaekwon <ohtk1119@gmail.com>
2024-03-22 13:58:20 +0900

    feat Connect Router config

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+// Configs
+import routerConfig from '@configs/router.config';
 
 const App = () => {
-  return <></>;
+  return <RouterProvider router={routerConfig} />;
 };
 
 export default App;
