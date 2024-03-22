2051216ff6d7f6dfabe514768aba5fd95dbab011
ohtaekwon <ohtk1119@gmail.com>
2024-03-22 13:53:38 +0900

    feat temporary implementation dummy DTO

---
+++ src/dto/dummy.dto.ts
@@ -0,0 +1,14 @@
+/**
+ * @type Dummy 데이터 DTO
+ *
+ * @description  id       {ID}               = 아이디
+ * @description  name     {string}           = 이름
+ * @description  country   {string}          = 나라
+ * @description  lang     {string}           = 언어
+ */
+export interface DummyDTO {
+  id: ID;
+  name: string;
+  country: string;
+  lang: string;
+}
