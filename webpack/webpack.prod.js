7bcb08691f47f66d522551b3b4def2c489b1031b
ohtaekwon <ohtk1119@gmail.com>
2024-03-27 13:29:27 +0900

    feat Add dotenv-wepback and plugins for each enviroment

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,9 @@
 const { merge } = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'hidden-source-map',
+  plugins: [new Dotenv({ path: '.env.productions' })],
 });
